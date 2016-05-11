769ee683572913542a181afb91720a7fb15da69d
TamTranAnymex <tam.tran@anymex.com>
2016-05-11 13:59:27 +0700

    App icon + Splash screen + some basics file

---
+++ MOJO-E/Extension.swift
@@ -0,0 +1,193 @@
+
+//
+//  MOJO-E
+//
+//  Created by Tam Tran on 5/11/16.
+//  Copyright © 2016 MOJO. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+
+extension String
+{
+    func trim() -> String {
+        return self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+    }
+    
+    func count() -> Int {
+        return self.characters.count
+    }
+    
+    func isValidEmail() -> Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}", options: .CaseInsensitive)
+            return regex.firstMatchInString(self, options: NSMatchingOptions(rawValue: 0), range: NSMakeRange(0, self.characters.count)) != nil
+        } catch {
+            return false
+        }
+    }
+}
+
+extension NSDate
+{
+    func day() -> Int {
+        //Get Day
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.Day, fromDate: self)
+        return components.day
+    }
+    func month() -> Int {
+        //Get Month
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.Month, fromDate: self)
+        return components.month
+    }
+    
+    
+    func hour() -> Int
+    {
+        //Get Hour
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.Hour, fromDate: self)
+        let hour = components.hour
+        //Return Hour
+        return hour
+    }
+    
+    func minute() -> Int
+    {
+        //Get Minute
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.Minute, fromDate: self)
+        let minute = components.minute
+        //Return Minute
+        return minute
+    }
+    
+    func year() -> Int
+    {
+        //Get Minute
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.Year, fromDate: self)
+        let year = components.year
+        //Return Minute
+        return year
+    }
+    
+    func toShortTimeString() -> String
+    {
+        //Get Short Time String
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "hh:mm a"
+        let timeString = formatter.stringFromDate(self)
+        //Return Short Time String
+        return timeString
+    }
+    
+    func nextDay() -> NSDate {
+        let tomorrow = NSCalendar.currentCalendar().dateByAddingUnit (
+            .Day,
+            value: 1,
+            toDate: self,
+            options: NSCalendarOptions(rawValue: 0))
+        return tomorrow!
+    }
+    
+    class func stringToDateOption(strDate: String!, format: String = "EEEE, MMMM dd, yyyy") -> NSDate? {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = format
+        return dateFormatter.dateFromString(strDate)
+    }
+    
+    func toShortTime() -> String {
+        let dateFormat = NSDateFormatter()
+        dateFormat.dateFormat = "HH:mm"
+        let strDateChanged = dateFormat.stringFromDate(self)
+        return strDateChanged
+    }
+    
+    func yearsFrom(date:NSDate) -> Int {
+        return NSCalendar.currentCalendar().components(.Year, fromDate: date, toDate: self, options: []).year
+    }
+    
+    func monthsFrom(date:NSDate) -> Int {
+        return NSCalendar.currentCalendar().components(.Month, fromDate: date, toDate: self, options: []).month
+    }
+    
+    func weeksFrom(date:NSDate) -> Int {
+        return NSCalendar.currentCalendar().components(.WeekOfYear, fromDate: date, toDate: self, options: []).weekOfYear
+    }
+    
+    func daysFrom(date:NSDate) -> Int {
+        return NSCalendar.currentCalendar().components(.Day, fromDate: date, toDate: self, options: []).day
+    }
+    
+    func hoursFrom(date:NSDate) -> Int {
+        return NSCalendar.currentCalendar().components(.Hour, fromDate: date, toDate: self, options: []).hour
+    }
+    
+    func minutesFrom(date:NSDate) -> Int {
+        return NSCalendar.currentCalendar().components(.Minute, fromDate: date, toDate: self, options: []).minute
+    }
+    
+    func secondsFrom(date:NSDate) -> Int {
+        return NSCalendar.currentCalendar().components(.Second, fromDate: date, toDate: self, options: []).second
+    }
+   
+    func offsetFrom(date:NSDate) -> String {
+        if yearsFrom(date)   > 0 { return "\(yearsFrom(date))y"   }
+        if monthsFrom(date)  > 0 { return "\(monthsFrom(date))M"  }
+        if weeksFrom(date)   > 0 { return "\(weeksFrom(date))w"   }
+        if daysFrom(date)    > 0 { return "\(daysFrom(date))d"    }
+        if hoursFrom(date)   > 0 { return "\(hoursFrom(date))h"   }
+        if minutesFrom(date) > 0 { return "\(minutesFrom(date))m" }
+        if secondsFrom(date) > 0 { return "\(secondsFrom(date))s" }
+        return ""
+    }
+}
+
+extension UIImage {
+    public func imageRotatedByDegrees(degrees: CGFloat, flip: Bool) -> UIImage {
+        let radiansToDegrees: (CGFloat) -> CGFloat = {
+            return $0 * (180.0 / CGFloat(M_PI))
+        }
+        let degreesToRadians: (CGFloat) -> CGFloat = {
+            return $0 / 180.0 * CGFloat(M_PI)
+        }
+        
+        // calculate the size of the rotated view's containing box for our drawing space
+        let rotatedViewBox = UIView(frame: CGRect(origin: CGPointZero, size: size))
+        let t = CGAffineTransformMakeRotation(degreesToRadians(degrees));
+        rotatedViewBox.transform = t
+        let rotatedSize = rotatedViewBox.frame.size
+        
+        // Create the bitmap context
+        UIGraphicsBeginImageContext(rotatedSize)
+        let bitmap = UIGraphicsGetCurrentContext()
+        
+        // Move the origin to the middle of the image so we will rotate and scale around the center.
+        CGContextTranslateCTM(bitmap, rotatedSize.width / 2.0, rotatedSize.height / 2.0);
+        
+        //   // Rotate the image context
+        CGContextRotateCTM(bitmap, degreesToRadians(degrees));
+        
+        // Now, draw the rotated/scaled image into the context
+        var yFlip: CGFloat
+        
+        if(flip){
+            yFlip = CGFloat(-1.0)
+        } else {
+            yFlip = CGFloat(1.0)
+        }
+        
+        CGContextScaleCTM(bitmap, yFlip, -1.0)
+        CGContextDrawImage(bitmap, CGRectMake(-size.width / 2, -size.height / 2, size.width, size.height), CGImage)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+}
---
+++ MOJO-E/Utility.swift
@@ -0,0 +1,139 @@
+//
+//  MOJO-E
+//
+//  Created by Tam Tran on 5/11/16.
+//  Copyright © 2016 MOJO. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SystemConfiguration
+
+class Utility {
+    
+    class func borderRadiusView(radius : CGFloat, view : UIView) {
+        view.layer.cornerRadius = radius
+        view.layer.masksToBounds = true
+    }
+    
+    class func showAlertWithMessage(message: String) {
+        dispatch_async(dispatch_get_main_queue(),{
+           let alert = UIAlertView(title: "Info", message: message, delegate: nil, cancelButtonTitle: "OK")
+            alert.show()
+            
+        })
+    }
+        
+    class func showAlertWithMessageOKCancel(message: String,title: String,sender: UIViewController, doneAction: ( () -> Void )?, cancelAction: ( () -> Void )? ) {
+        dispatch_async(dispatch_get_main_queue(), {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+            let okAction: UIAlertAction = UIAlertAction(title: "OK", style: .Default) { action -> Void in
+                if let action = doneAction {
+                    action()
+                }
+            }
+            alert.addAction(okAction)
+            let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel) {
+                action -> Void in
+                if let action = cancelAction {
+                    action()
+                }
+            }
+            alert.addAction(cancelAction)
+            sender.presentViewController(alert, animated: true, completion: { () in  })
+        })
+    }
+    
+    
+    class func navigateToVC(from : UIViewController, vc : UIViewController, direction : String = kCATransitionFromRight) {
+        
+        vc.view.frame = from.view.frame
+        from.addChildViewController(vc)
+        from.view.addSubview(vc.view)
+        // Move the view move in from the right
+        CATransaction.begin()
+        CATransaction.setAnimationDuration(0.3)
+        let transition = CATransition()
+        transition.type = kCATransitionPush
+        transition.subtype = direction
+        vc.view.layer .addAnimation(transition, forKey: kCATransition)
+        vc.didMoveToParentViewController(from)
+        CATransaction.commit()
+    }
+    
+    class func openAuthenticatedFlow() {
+        let authenticationSB = UIStoryboard(name: "Authenticated", bundle: NSBundle.mainBundle())
+        let initVC = authenticationSB.instantiateInitialViewController()
+        appDelegate.window?.rootViewController = initVC
+    }
+    
+    class func openAuthenticationFlow() {
+        let authenticationSB = UIStoryboard(name: "Authentication", bundle: NSBundle.mainBundle())
+        let initVC = authenticationSB.instantiateInitialViewController()
+        appDelegate.window?.rootViewController = initVC
+    }
+    
+//    class func getSideMenuNavigationC() -> UISideMenuNavigationController {
+//        let authenticationSB = UIStoryboard(name: "Authenticated", bundle: NSBundle.mainBundle())
+//        let sideMenuNavigationC = authenticationSB.instantiateViewControllerWithIdentifier("SideMenuNavigationControllerID") as! UISideMenuNavigationController
+//        return sideMenuNavigationC
+//    }
+    
+    class func greenL1Color() -> UIColor {
+        return UIColor(red: 139.0/255.0, green: 195.0/255.0, blue: 74.0/255.0, alpha: 1.0)
+    }
+    
+    class func greenL2Color() -> UIColor {
+        return UIColor(red: 156.0/255.0, green: 204.0/255.0, blue: 101.0/255.0, alpha: 1.0)
+    }
+    
+    class func greenL3Color() -> UIColor {
+        return UIColor(red: 174.0/255.0, green: 213.0/255.0, blue: 129.0/255.0, alpha: 1.0)
+    }
+    
+    class func scaleImage(image: UIImage, toSize newSize: CGSize) -> (UIImage) {
+        let newRect = CGRectIntegral(CGRectMake(0,0, newSize.width, newSize.height))
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
+        let context = UIGraphicsGetCurrentContext()
+        CGContextSetInterpolationQuality(context, .High)
+        let flipVertical = CGAffineTransformMake(1, 0, 0, -1, 0, newSize.height)
+        CGContextConcatCTM(context, flipVertical)
+        CGContextDrawImage(context, newRect, image.CGImage)
+        let newImage = UIImage(CGImage: CGBitmapContextCreateImage(context)!)
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+    
+    class func nsData2UIImage(data: NSData) -> UIImage? {
+        return UIImage(data: data)
+    }
+    
+    
+    class func backgroundThread(delay: Double = 0.0, background: (() -> Void)? = nil, completion: (() -> Void)? = nil) {
+        dispatch_async(dispatch_get_global_queue(Int(QOS_CLASS_USER_INITIATED.rawValue), 0)) {
+            if(background != nil){ background!(); }
+            
+            let popTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC)))
+            dispatch_after(popTime, dispatch_get_main_queue()) {
+                if(completion != nil){ completion!(); }
+            }
+        }
+    }
+    
+    //MARK: Check internet connection
+    class func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
+            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+        }
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
+    }
+}
